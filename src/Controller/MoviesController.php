64d173a60db26b570b806442b6f2ce28805651c2
Branislav Obrtáč <brano.obrtac@gmail.com>
2022-09-14 12:18:01 +0200

    Created new structure. Added CRUD files. Fixed index and show file. Copied css from github :/ (will make my own in future)

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,24 +12,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em)
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository)
     {
-        $this->em = $em;
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'movies')]
+    #[Route('/movies', methods: ['GET'], name: 'movies')]
     public function index(): Response
     {
+        $movies = $this->movieRepository->findAll();
+
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
+    }
+
+    #[Route('/movies/{id}', methods: ['GET'], name: 'movies-show')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
+    }
+}
+
         // findAll() - SELECT * FROM movies
         // find() - SELECT * FROM movies WHERE id = 5
         // findBy() - SELECT * FROM movies ORDER BY id DESC
         // findOneBy() - SELECT * FROM movies WHERE id = 6 AND title = 'Thor: Love and Thunder' ORDER BY id DESC
         // count() - SELECT COUNT() from movies WHERE id = 1
-
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->getClassName();
-
-        return $this->render('index.html.twig');
-    }
-}
